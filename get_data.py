127f091cae25dcef750f68388d31c47b2473a99e
Nico Grosch <nicogr1980@gmail.com>
2026-06-18 00:14:15 +0200

    added process driver function for dynamic driver data fetching

--- get_data.py
+++ get_data.py
@@ -1,5 +1,6 @@
 import fastf1
 import os
+import pandas as pd
 
 if not os.path.exists('cache_data'):
     os.mkdir('cache_data')
@@ -12,14 +13,20 @@ class GetData:
         self.driver_1 = driver_1
         self.driver_2 = driver_2
 
+    def prcess_drivers(self, argument, *drivers):
+        results = {}
+        for f in drivers:
+            var = argument(f)
+            results[f] = var
+        return  results
+
     def data(self):
         session = fastf1.get_session(year=2026, gp="Australia", identifier='Qualifying')
         session.load()
 
-        # Gets ALL Lap times for specific driver //Both use driver call sign
-        driver_1_all_laps = session.laps.pick_driver(self.driver_1)
-        driver_2_all_laps = session.laps.pick_driver(self.driver_2)
+        # Gets ALL Lap times for specific drivers //Both use driver call sign
+        driver_data = self.prcess_drivers(session.laps.pick_drivers, self.driver_1, self.driver_2)
 
-        # Gets FASTEST Lap for specific driver
-        driver_1_fastest_lap = driver_1_all_laps.pick_fastest(self.driver_1)
-        driver_2_fastest_lap = driver_2_all_laps.pick_fastest(self.driver_2)
+        # Gets FASTEST Lap for drivers
+
+GetData("ANT", "HAM").data()
