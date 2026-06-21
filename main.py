f5b75d80233d989958c4059f1b7ff782e4f77731
Nico Grosch <nicogr1980@gmail.com>
2026-06-21 22:59:52 +0200

    Added intro cli page

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from textual.app import App, ComposeResult
-from textual.containers import Container
-from textual.widgets import Label, Button
+from textual.widgets import Label, Static
 from textual.screen import Screen
 from textual import events
 
+def ascii_art(filename : str):
+    try:
+        with open("ascii arts/" + filename) as file:
+            return str(file.read())
+    except FileNotFoundError:
+        return "ASCII ART NOT FOUND"
+
 class Intro(Screen):
     CSS_PATH = "style.tcss"
-    def compose(self) -> ComposeResult:
-        yield Label("F1 CLI 2026", id="intro")
-        yield Label("PRESS [ENTER] TO CONTINUE", id="hint")
 
-    def on_key(self, event: events.KeyEvent) -> None:
+    def compose(self) -> ComposeResult:
+        yield Label(ascii_art("intro.txt"), id="intro")
+        yield Label("PRESS \\[ENTER] TO CONTINUE", id="hint")
+
+    def on_key(self, event: events.Key) -> None:
         if event.key == "enter":
             self.app.exit()
 
