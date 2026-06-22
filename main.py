77c1f59ebf1610bb0761114db78b8857acb93cd6
Nico Grosch <nicogr1980@gmail.com>
2026-06-22 15:05:34 +0200

    added bento box grid system for cli layout & added ascii art and title to the header

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from textual.app import App, ComposeResult
 from textual.widgets import Label, Static
 from textual.screen import Screen
 from textual import events
+from textual.containers import Grid, Container, Horizontal
 
 def ascii_art(filename : str):
     try:
@@ -14,16 +15,35 @@ class Intro(Screen):
     CSS_PATH = "style.tcss"
 
     def compose(self) -> ComposeResult:
-        yield Label(ascii_art("intro.txt"), id="intro")
+        yield Label(ascii_art("logo.txt"), id="intro")
         yield Label("PRESS \\[ENTER] TO CONTINUE", id="hint")
 
     def on_key(self, event: events.Key) -> None:
         if event.key == "enter":
             self.app.exit()
 
+class Dashboard(Screen):
+    CSS_PATH = "style.tcss"
+
+    def compose(self) -> ComposeResult:
+        with Grid(id="header"):
+            yield Label(ascii_art("logo.txt"), id="logo")
+            yield Label("\nF1 CLI DASHBOARD", id = "header-text")
+
+        with Grid(id="first-grid"):
+            yield Static(classes="bento-box")
+            yield Static(classes="bento-box")
+        with Grid(id="second-grid"):
+            yield Static(classes="bento-box")
+            yield Static(classes="bento-box")
+
+        with Container(id="footer"):
+            yield Label("F1 CLI | CURRENT_RACE: $DAYS | PRESS \\[1-7] FOR MENU | (q) QUIT", id="footer-text")
+
+
 class F1App(App):
     def on_mount(self) -> None:
-        self.push_screen(Intro())
+        self.push_screen(Dashboard())
 
 if __name__ == "__main__":
     F1App().run()
